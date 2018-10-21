2858a4c7a20e6562d854c58fcdffa8c52363679e
Meandester <filipsek@live.com>
2018-10-21 11:34:09 +0200

    array list cwiczenie zakonczone

--- src/com/sda/kolekcje/Main.java
+++ src/com/sda/kolekcje/Main.java
@@ -7,44 +7,44 @@ public class Main {
 
     public static void main(String[] args) {
         /**
-        List<String> lista = new ArrayList<>();
+         List<String> lista = new ArrayList<>();
 
-        lista.add("Jan");
-        lista.add("Maciej");
-        lista.add("Tadeusz");
+         lista.add("Jan");
+         lista.add("Maciej");
+         lista.add("Tadeusz");
 
-        System.out.println("Rozmiar: " + lista.size());
+         System.out.println("Rozmiar: " + lista.size());
 
-        lista.add("Mateusz");
-        lista.add("Karol");
+         lista.add("Mateusz");
+         lista.add("Karol");
 
-        System.out.println("Rozmiar: " + lista.size());
+         System.out.println("Rozmiar: " + lista.size());
 
-        for (String imie: lista){
-            System.out.println("Imie: " + imie);
-        }
+         for (String imie: lista){
+         System.out.println("Imie: " + imie);
+         }
 
-        for (int i = 0; i < lista.size() ; i++) {
-            System.out.println("Imie: " + lista.get(i));
-        }
+         for (int i = 0; i < lista.size() ; i++) {
+         System.out.println("Imie: " + lista.get(i));
+         }
 
-        lista.remove("Jan");
-        lista.remove(0);
-        try {
-            String imie = lista.get(2);
-            System.out.println("Element o indeksie 2: " + imie);
-        } catch (IndexOutOfBoundsException e){
-            System.out.println("Nie ma takiego elementu tablicy");
-            e.printStackTrace();
-        }
+         lista.remove("Jan");
+         lista.remove(0);
+         try {
+         String imie = lista.get(2);
+         System.out.println("Element o indeksie 2: " + imie);
+         } catch (IndexOutOfBoundsException e){
+         System.out.println("Nie ma takiego elementu tablicy");
+         e.printStackTrace();
+         }
 
 
-        lista.clear(); // czyszczenie listy z elementów
+         lista.clear(); // czyszczenie listy z elementów
          */
 
-        User user1 = new User("Anna","Dąb",15);
-        User user2 = new User("Kasia","Kowalska",25);
-        User user3 = new User("Maria","Kowalczyk",50);
+        User user1 = new User("Anna", "Dąb", 15);
+        User user2 = new User("Kasia", "Kowalska", 25);
+        User user3 = new User("Maria", "Kowalczyk", 50);
 
         List<User> userList = new ArrayList<>();
 
@@ -52,11 +52,16 @@ public class Main {
         userList.add(user2);
         userList.add(user3);
 
-        for (User lista: userList){
+        for (User lista : userList) {
             System.out.println("Imie: " + lista.getName() + " Nazwisko: " + lista.getLastname() + " Wiek: " + lista.getAge());
         }
-        for (User lista: userList){
+        for (User lista : userList) {
             System.out.println(lista.toString());
+        }
+
+        for (int i = 0; i <userList.size() ; i++) {
+            System.out.println(userList.get(i));
+
         }
 
 
