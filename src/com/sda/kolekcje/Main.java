e53c838c211dd9a32475236446992b1b6bd0619f
Meandester <filipsek@live.com>
2018-10-21 11:00:15 +0200

    dodana klasa user i arraylist

--- src/com/sda/kolekcje/Main.java
+++ src/com/sda/kolekcje/Main.java
@@ -1,8 +1,64 @@
 package com.sda.kolekcje;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        /**
+        List<String> lista = new ArrayList<>();
+
+        lista.add("Jan");
+        lista.add("Maciej");
+        lista.add("Tadeusz");
+
+        System.out.println("Rozmiar: " + lista.size());
+
+        lista.add("Mateusz");
+        lista.add("Karol");
+
+        System.out.println("Rozmiar: " + lista.size());
+
+        for (String imie: lista){
+            System.out.println("Imie: " + imie);
+        }
+
+        for (int i = 0; i < lista.size() ; i++) {
+            System.out.println("Imie: " + lista.get(i));
+        }
+
+        lista.remove("Jan");
+        lista.remove(0);
+        try {
+            String imie = lista.get(2);
+            System.out.println("Element o indeksie 2: " + imie);
+        } catch (IndexOutOfBoundsException e){
+            System.out.println("Nie ma takiego elementu tablicy");
+            e.printStackTrace();
+        }
+
+
+        lista.clear(); // czyszczenie listy z elementów
+         */
+
+        User user1 = new User("Anna","Dąb",15);
+        User user2 = new User("Kasia","Kowalska",25);
+        User user3 = new User("Maria","Kowalczyk",50);
+
+        List<User> userList = new ArrayList<>();
+
+        userList.add(user1);
+        userList.add(user2);
+        userList.add(user3);
+
+        for (User lista: userList){
+            System.out.println("Imie: " + lista.getName() + " Nazwisko: " + lista.getLastname() + " Wiek: " + lista.getAge());
+        }
+        for (User lista: userList){
+            System.out.println(lista.toString());
+        }
+
+
     }
 }
---
+++ src/com/sda/kolekcje/User.java
@@ -0,0 +1,45 @@
+package com.sda.kolekcje;
+
+public class User {
+    private String name;
+    private String lastname;
+    private int age;
+
+    public User (String name, String lastname, int age){
+        this.name=name;
+        this.lastname=lastname;
+        this.age=age;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
